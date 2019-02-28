036a559277cb1c2ed53527aecae0a9d8f541bf17
Lige <lige.williamson@gmail.com>
2019-02-28 15:28:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,6 +8,7 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if pigeon_list.has_key?(name)
           if pigeon_list[name].has_key?(category)
+<<<<<<< HEAD
             pigeon_list[name][category] << attribute.to_s
           else
             pigeon_list[name][category] = [attribute.to_s]
@@ -15,9 +16,22 @@ def nyc_pigeon_organizer(data)
         else
           pigeon_list[name] = {}
           pigeon_list[name][category] = [attribute.to_s]
+=======
+            pigeon_list[name][category] << attribute
+          else
+            pigeon_list[name][category] = [attribute]
+          end
+        else
+          pigeon_list[name] = category
+          pigeon_list[name][category] = [attribute]
+>>>>>>> 4b3c66a15f1627e38160c4b8730924eb72ecfe6f
         end
       end
     end
   end
+<<<<<<< HEAD
+=======
+  binding.pry
+>>>>>>> 4b3c66a15f1627e38160c4b8730924eb72ecfe6f
   pigeon_list
 end
